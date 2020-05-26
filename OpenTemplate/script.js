482dd6ddc4187a2410268936c1fbc94b37ba3334
VitaliqMetaliq <60176142+VitaliqMetaliq@users.noreply.github.com>
2020-05-26 15:45:19 +0300

    adapt for template

--- OpenTemplate/script.js
+++ OpenTemplate/script.js
@@ -2,6 +2,19 @@ $(function(){
     $('.link-to-scroll').on('click', function(e){
         $('html,body').stop().animate({ 
             scrollTop: $(this.hash).offset().top }, 1000);
+        if($('.header-nav').hasClass('header-nav-open')) {
+            $('.header-nav').removeClass('header-nav-open');
+        }
         e.preventDefault();
+    });
+
+    $('.header-burger').on('click', function(e) {
+        e.preventDefault();
+        $('.header-nav').addClass('header-nav-open');
+    });
+
+    $('.header-nav-close').on('click', function(e){
+        e.preventDefault();
+        $('.header-nav').removeClass('header-nav-open');
     });
 });
